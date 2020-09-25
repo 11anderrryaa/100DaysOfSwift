f8b08f65732441b8542a5cb638fdcb552ab3637c
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2020-09-25 08:26:38 -0600

    Days 16 - 18

---
+++ Day 16/day16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day16
+//
+//  Created by Ryan Anderson on 9/15/20.
+//  Copyright © 2020 Ryan Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
