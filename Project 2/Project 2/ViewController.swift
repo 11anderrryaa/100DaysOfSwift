ffb7fcdd18649a5744d19f8ce00c92190bb97b55
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2020-10-04 18:54:10 -0600

    Day 19

--- Project 2/Project 2/ViewController.swift
+++ Project 2/Project 2/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var scores = 0
+    var correctAnswer = 0
+    var numberOfGuesses = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +33,47 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         buttone2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+         title = "Guess:\(countries[correctAnswer].uppercased()) Score:\(scores)"
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            scores += 1
+            numberOfGuesses += 1
+            if numberOfGuesses == 11 {
+                return
+            }
+        } else {
+            title = "Wrong"
+            scores -= 1
+            numberOfGuesses += 1
+        }
+        
+        if numberOfGuesses < 11 { let ac = UIAlertController(title: title, message: "Your score is \(scores) out of \(numberOfGuesses).", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(scores) out of \(numberOfGuesses).", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+        }
+    }
+    
 }
 
